916b00241dad61fe75c42e153b937fe747220415
unknown <mfaqihuddin23@gmail.com>
2023-08-23 17:01:37 +0700

    tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import eslintPlugin from 'vite-plugin-eslint';
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      eslintPlugin()
+    ]
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
